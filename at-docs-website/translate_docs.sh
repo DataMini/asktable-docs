9d4fb994d3f8939c68c229a8ca11b627cc1b5272
lele <lele@datamini.ai>
2025-01-26 19:10:54 +0800

    update translate script

--- at-docs-website/translate_docs.sh
+++ at-docs-website/translate_docs.sh
@@ -6,7 +6,7 @@ source .env
 
 NODE_OPTIONS=--no-deprecation node --trace-deprecation translate_docs.js \
 --inputDir ./docs \
---outputDir ./i18n/en/docusaurus-plugin-content-docs/current2 \
+--outputDir ./i18n/en/docusaurus-plugin-content-docs/current \
 --targetLanguage english \
 --concurrency 1 \
 --model qwen-turbo
